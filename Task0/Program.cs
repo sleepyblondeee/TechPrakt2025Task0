23dd7a654936d976f58679780d23e1a076b570e4
Kateryna Nechyporuk <vt231_nko@student.ztu.edu.ua>
2025-01-30 17:13:02 +0200

    Task1

---
+++ Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
+        Console.Write("Введіть перше число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума: {sum}");
+    }
+}
